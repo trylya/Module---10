75f4ea4e85be90277434f16aad70ffe1786ea224
Лена <trylya@mail.ru>
2024-11-24 20:50:03 +0300

    Введение в потоки

---
+++ module_10_1.py
@@ -0,0 +1,92 @@
+import time
+from time import sleep
+from threading import Thread
+from datetime import datetime
+time_start = datetime.now()
+
+def wite_words(word_count, file_name):
+        with open(file_name, 'w', encoding='utf-8') as file:
+            for i in range(1, word_count + 1):
+                file.write(f'Какое-то слово № {i}\n')
+                time.sleep(0.1)
+        return print(f'Завершилась запись в файл {file_name}')
+
+    # вызов функции
+wite_words(10, 'example1.txt')
+wite_words(30, 'example2.txt')
+wite_words(200, 'example3.txt')
+wite_words(100, 'example4.txt')
+
+time_end_1 = datetime.now()
+time_res = time_end_1 - time_start
+print(f'Работа потоков {time_res}')
+
+    # вызов потоков
+thr_1 = Thread(target=wite_words, args=(10, 'example5.txt'))
+thr_2 = Thread(target=wite_words, args=(30, 'example6.txt'))
+thr_3 = Thread(target=wite_words, args=(200, 'example7.txt'))
+thr_4 = Thread(target=wite_words, args=(100, 'example8.txt'))
+
+thr_1.start()
+thr_2.start()
+thr_3.start()
+thr_4.start()
+
+thr_1.join()
+thr_2.join()
+thr_3.join()
+thr_4.join()
+
+time_end_2 = datetime.now()
+time_res_2 = time_end_2 - time_start
+
+print(f'Работа потоков {time_res_2 - time_res}')
+
+# Цель: понять как работают потоки на практике, решив задачу
+#
+# Задача "Потоковая запись в файлы":
+# Необходимо создать функцию write_words(word_count, file_name), где word_count - количество
+# записываемых слов, file_name - название файла, куда будут записываться слова.
+# Функция должна вести запись слов "Какое-то слово № <номер слова по порядку>" в соответствующий
+# файл с прерыванием после записи каждого на 0.1 секунду.
+# Сделать паузу можно при помощи функции sleep из модуля time,
+# предварительно импортировав её: from time import sleep.
+# В конце работы функции вывести строку "Завершилась запись в файл <название файла>".
+#
+# После создания файла вызовите 4 раза функцию write_words, передав в неё следующие значения:
+# 10, example1.txt
+# 30, example2.txt
+# 200, example3.txt
+# 100, example4.txt
+# После вызовов функций создайте 4 потока для вызова этой функции со следующими аргументами для функции:
+# 10, example5.txt
+# 30, example6.txt
+# 200, example7.txt
+# 100, example8.txt
+# Запустите эти потоки методом start не забыв, сделать остановку основного потока при помощи join.
+# Также измерьте время затраченное на выполнение функций и потоков.
+# Как это сделать рассказано в лекции к домашнему заданию.
+#
+# Пример результата выполнения программы:
+# Алгоритм работы кода:
+# # Импорты необходимых модулей и функций
+# # Объявление функции write_words
+# # Взятие текущего времени
+# # Запуск функций с аргументами из задачи
+# # Взятие текущего времени
+# # Вывод разницы начала и конца работы функций
+# # Взятие текущего времени
+# # Создание и запуск потоков с аргументами из задачи
+# # Взятие текущего времени
+# # Вывод разницы начала и конца работы потоков
+# Вывод на консоль:
+# Завершилась запись в файл example1.txt
+# Завершилась запись в файл example2.txt
+# Завершилась запись в файл example3.txt
+# Завершилась запись в файл example4.txt
+# Работа потоков 0:00:34.003411 # Может быть другое время
+# Завершилась запись в файл example5.txt
+# Завершилась запись в файл example6.txt
+# Завершилась запись в файл example8.txt
+# Завершилась запись в файл example7.txt
+# Работа потоков 0:00:20.071575 # Может быть другое время
